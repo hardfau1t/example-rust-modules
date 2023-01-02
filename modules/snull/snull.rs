1a4d14fe0197935783d45330d0a13738c170350e
hardfau18 <the.qu1rky.b1t@gmail.com>
2023-01-02 08:42:40 +0530

    transmit and recieve are done, but not working

--- modules/snull/snull.rs
+++ modules/snull/snull.rs
@@ -5,6 +5,7 @@
 
 use core::slice;
 
+use kernel::bindings::{ethhdr, iphdr, netif_rx, skb_clone};
 use kernel::net::device::{Flag, NetdevTX};
 use kernel::net::prelude::{EthToolOps, NetDevice, NetDeviceAdapter, NetDeviceOps, SkBuff};
 use kernel::net::rtnl_link_ops;
@@ -19,6 +20,38 @@ module! {
     license: "GPL",
 }
 
+/// updates the buffer so that it can be retransmitted on the same port
+fn update_skb(skb: &mut SkBuff, dev: *const kernel::bindings::net_device) {
+    let buf = skb.get_internal().data;
+    let eth_hdr_size = core::mem::size_of::<ethhdr>();
+    let ip_hdr = &mut unsafe { *(buf.add(eth_hdr_size) as *mut iphdr) };
+    ip_hdr.saddr ^= 1 << 16;
+    ip_hdr.daddr ^= 1 << 16;
+    ip_hdr.check = 0;
+    //TODO: Update checksum
+    unsafe{(&mut *(skb.get_pointer() as *mut kernel::bindings::sk_buff)).__bindgen_anon_1.__bindgen_anon_1.__bindgen_anon_1.dev = dev as *mut _};
+}
+
+fn disp_packet(skb: &SkBuff) {
+    let len = skb.len();
+    let internal = skb.get_internal();
+    let tail = internal.tail;
+    let end = internal.end;
+    let data_len = internal.data_len;
+    let data: &[u8] = unsafe { slice::from_raw_parts(internal.data, len.try_into().unwrap()) };
+    // pr_err!("Not Implement: start_xmit\n");
+    // pr_info!("tail {tail:?}, end {end:?}, len {len:?}, data_len {data_len:?}\n");
+    pr_info!("destination: {:x?}\n", &data[..6]);
+    pr_info!("source: {:x?}\n", &data[6..12]);
+    pr_info!("ethertype: {:x?}\n", &data[12..14]);
+    if data[12] == 0x86 && data[13] == 0xdd {
+        print_ipv6(&data[14..data.len() - 4]);
+    } else {
+        pr_info!("unknown packet: {:x?}\n", &data[14..data.len() - 4]);
+    }
+    pr_info!("crc: {:x?}\n", &data[data.len() - 4..]);
+}
+
 fn print_ipv6(data: &[u8]) {
     if data.len() < 40 {
         pr_err!("ipv6 buffer with less than 40 bytes\n");
@@ -26,8 +59,14 @@ fn print_ipv6(data: &[u8]) {
     }
     pr_info!("IPV6 packet:\n");
     pr_info!("\tversion: {:x?}\n", data[0] >> 4);
-    pr_info!("\ttraffic class: {:x?}\n", (data[0] & ((1 << 4) - 1)) | (data[1]>>4));
-    pr_info!("\tflow label: {:x?}\n", [data[1] & ((1<< 4)-1), data[2], data[3]]);
+    pr_info!(
+        "\ttraffic class: {:x?}\n",
+        (data[0] & ((1 << 4) - 1)) | (data[1] >> 4)
+    );
+    pr_info!(
+        "\tflow label: {:x?}\n",
+        [data[1] & ((1 << 4) - 1), data[2], data[3]]
+    );
     pr_info!("\tpayload length: {:x?}\n", &data[4..6]);
     pr_info!("\tnext header: {:x?}\n", data[6]);
     pr_info!("\thop limit: {:x?}\n", data[7]);
@@ -40,7 +79,13 @@ struct Snull {
     sn0: NetDevice<SnullPriv>,
     sn1: NetDevice<SnullPriv>,
 }
-struct SnullPriv;
+struct SnullPriv {
+    // TODO: later change this to mem::Uninit
+    sibling: Option<*const kernel::bindings::net_device>,
+}
+
+unsafe impl Sync for SnullPriv {}
+unsafe impl Send for SnullPriv {}
 impl NetDeviceAdapter for SnullPriv {
     type Inner = Self;
 
@@ -67,25 +112,13 @@ impl NetDeviceOps<SnullPriv> for SnullPriv {
         // pr_info!("Snull Device is deinitialized\n");
     }
 
-    fn start_xmit(skb: SkBuff, dev: &mut NetDevice<Self>) -> NetdevTX {
-        let len = skb.len();
-        let internal = skb.get_internal();
-        let tail = internal.tail;
-        let end = internal.end;
-        let data_len = internal.data_len;
-        let data: &[u8] = unsafe { slice::from_raw_parts(internal.data, len.try_into().unwrap()) };
-        // pr_err!("Not Implement: start_xmit\n");
-        // pr_info!("tail {tail:?}, end {end:?}, len {len:?}, data_len {data_len:?}\n");
-        pr_info!("destination: {:x?}\n", &data[..6]);
-        pr_info!("source: {:x?}\n", &data[6..12]);
-        pr_info!("ethertype: {:x?}\n", &data[12..14]);
-        if data[12] == 0x86 && data[13] == 0xdd{
-        print_ipv6(&data[14..data.len() - 4]);
-        }else{
-            pr_info!("unknown packet: {:x?}\n", &data[14..data.len() - 4]);
-        }
-        pr_info!("crc: {:x?}\n", &data[data.len() - 4..]);
-        drop(skb);
+    fn start_xmit(mut skb: SkBuff, dev: &mut NetDevice<Self>) -> NetdevTX {
+        // disp_packet(&skb);
+        let priv_data = dev.get_priv_data();
+        update_skb(&mut skb, priv_data.sibling.unwrap());
+        if  unsafe { netif_rx(skb_clone(skb.get_pointer() as *mut _, 0)) } !=0{
+            pr_err!("Packed dropped\n");
+        };
         NetdevTX::TX_OK
     }
     fn get_stats64(dev: &mut NetDevice<SnullPriv>, stats: &mut kernel::net::rtnl::RtnlLinkStats64) {
@@ -124,25 +157,28 @@ impl kernel::Module for Snull {
         // pr_info!("Snull Init \n");
         // pr_info!("Setting up dev1\n");
         let mut dev1 = NetDevice::new(
-            SnullPriv,
+            SnullPriv { sibling: None },
             kernel::c_str!("snull%d"),
             kernel::net::device::NetNameAssingType::Enum,
             1,
             1,
         )?;
+        // pr_info!("Setting up dev2\n");
+        let mut dev2 = NetDevice::new(
+            SnullPriv {
+                sibling: Some(dev1.get_internal() as *const _),
+            },
+            kernel::c_str!("snull%d"),
+            kernel::net::device::NetNameAssingType::Enum,
+            1,
+            1,
+        )?;
+        dev1.get_priv_data_mut().sibling = Some(dev2.get_internal() as *const _);
         // pr_info!("Registering up dev1\n");
         if let Err(e) = dev1.register() {
             pr_err!("Failed to register snull0\n");
             return Err(e);
         }
-        // pr_info!("Setting up dev2\n");
-        let mut dev2 = NetDevice::new(
-            SnullPriv,
-            kernel::c_str!("snull%d"),
-            kernel::net::device::NetNameAssingType::Enum,
-            1,
-            1,
-        )?;
         // pr_info!("registering up dev2\n");
         if let Err(e) = dev2.register() {
             pr_err!("Failed to register snull1\n");
